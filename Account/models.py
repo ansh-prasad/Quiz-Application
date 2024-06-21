36b9b7a5845584eff6768941c4f8f4a562d49839
ansh-prasad <anshprasad01@gmail.com>
2024-06-21 12:33:59 +0530

    Modela For Account

--- Account/models.py
+++ Account/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from  django.contrib.auth.models import User
 
 class Profile(models.Model):
-    username = models.CharField(max_length=50)
+    user = models.OneToOneField(User , on_delete=models.CASCADE , null=True)
+    email_address = models.CharField(max_length=50 , unique=True , null=True)
+    bio = models.TextField(blank=True , null=True)
+    profile_img = models.ImageField(upload_to='profile_images' , default='1.jpg' , blank=True , null=True)
+    location = models.CharField(max_length=100 , null=True , blank=True)
+    GENDER = (
+        ('Male' , 'Male'),
+        ('Female' , 'Female'),
+    )
+    gender = models.CharField(max_length=6 , choices=GENDER , blank=True , null=True)
