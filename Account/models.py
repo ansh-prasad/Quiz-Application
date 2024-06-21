26d34a563934042a730ec721e784d63dba0ab248
ansh-prasad <anshprasad01@gmail.com>
2024-06-21 17:18:57 +0530

    Register Page Backend

--- Account/models.py
+++ Account/models.py
@@ -2,13 +2,18 @@ from django.db import models
 from  django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User , on_delete=models.CASCADE , null=True)
+    user = models.OneToOneField(User , on_delete=models.CASCADE , null=True , verbose_name='User Object')
     email_address = models.CharField(max_length=50 , unique=True , null=True)
     bio = models.TextField(blank=True , null=True)
-    profile_img = models.ImageField(upload_to='profile_images' , default='1.jpg' , blank=True , null=True)
-    location = models.CharField(max_length=100 , null=True , blank=True)
+    profile_img = models.ImageField(upload_to='profile_images' , default='1.jpg' , blank=True , null=True ,  verbose_name='Profile Pic')
+    location = models.CharField(max_length=100 , null=False , blank=False)
     GENDER = (
         ('Male' , 'Male'),
         ('Female' , 'Female'),
     )
-    gender = models.CharField(max_length=6 , choices=GENDER , blank=True , null=True)
+    gender = models.CharField(max_length=6 , choices=GENDER , blank=False , null=False)
+    name = models.CharField(max_length=50 , null=True , blank=True )
+
+    def __str__(self):
+        return self.user.username
+    
--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth.models import User , auth
+from .models import Profile
 
 # Create your views here.
+def register(request):
+
+    if request.method == "POST":
+
+        username = request.POST['username']
+        name = request.POST['fullname']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm password']
+        
+        if User.objects.filter(username=username).exists():
+            messages.info(request , "Username Already Taken")
+            return redirect('register')
+        
+        elif User.objects.filter(email=email).exists():
+            messages.info(request , "Email Already Used")
+            return redirect('register')
+        
+        elif password != confirm_password:
+            messages.info(request , "Password Not Matching")
+            return redirect('register')
+        
+        else:
+            user = User.objects.create_user(username=username , email=email , password=password )
+            user.save()
+
+            user_login = auth.authanticate(username=username , pasaword=password)
+            auth.login(request , user_login)
+
+            user_model = User.objects.get(username=username)
+            new_profile = Profile.objects.create(user = user_model , email_address=email , name=name)
+            new_profile.save()
+
+        
+
+    context = {}
+    return render(request , 'register.html' , context)
